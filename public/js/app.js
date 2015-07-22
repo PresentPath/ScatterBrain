b054b06c0784448c034a04cbe7813af865446c7d
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-07-22 13:01:19 -0700

    Calculate x and y coordinates of nodes in network 1.

--- public/js/app.js
+++ public/js/app.js
@@ -38,19 +38,66 @@ $(document).ready(function() {
     $('#hiddenLayers').val('');
     console.log('train brains!');
 
-
   });
 
 });
 
+// Options to control display of networks
+var displayOptions = {
+  width: 960,
+  height: 640,
+  verticalOffset: 160,
+  margin: 20
+};
+
 // Render a network of nodes for neural network
 var drawNetworks = function() {
   // Parse the values in the input forms to get the array of nodes for each network
-  // Add element for nodes in input layer and output layer
+  // Add elements for nodes in input layer and output layer
   var network1NodeList = [2].concat($('#hiddenLayers1').val().split(',').map(Number),[0]);
   // Add one to each layer to account for bias nodes
   network1NodeList = network1NodeList.map(function(elem){
     return elem + 1;
   });
   console.log(network1NodeList);
+  
+  var separation = ((displayOptions.width/4) - (2 * displayOptions.margin)) / (network1NodeList.length-1);
+  console.log('horizontal separation:', separation);
+
+  // Calculate the x-coordinates for each layer in network 1
+  var network1XCoordinates = [];
+  
+  network1NodeList.forEach(function(elem, index) {
+    var xCoordinate = Math.round((index * separation) + displayOptions.margin);
+    network1XCoordinates.push(xCoordinate);
+  });
+
+  console.log(network1XCoordinates);
+
+  // Calculate the y-coordinates for each layer in network 1
+  var network1YCoordinates = [];
+  
+  network1NodeList.forEach(function(elem, index) {
+    // Each element represents a layer
+    // For each layer use the number of nodes in the layer to determine the separation between each node
+    var separation = ((displayOptions.height - (2 * displayOptions.margin)) / Math.max((elem - 1),2));
+    console.log('vertical separation:', separation);
+
+    // Generate the y-coordinate for each node in the layer
+    var layerYCoordinates = [];
+    for(var i = 0; i < elem; i++) {
+      if (elem === 1) {
+        i = 1;
+      }
+      var yCoordinate = Math.round((i * separation) + (displayOptions.verticalOffset + displayOptions.margin));
+      layerYCoordinates.push(yCoordinate);
+    }
+    console.log('layer', index+1);
+    console.log('y-coordinates for layer', layerYCoordinates);
+
+    network1YCoordinates.push(layerYCoordinates);
+  });
+
+  console.log('network1YCoordinates:', network1YCoordinates);
+
 };
