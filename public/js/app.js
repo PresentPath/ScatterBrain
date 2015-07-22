9edab619b969105d9f44707ff853d746a1295ddd
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-07-22 10:47:37 -0700

    Separate hidden layer input into separate forms and get array of number of nodes for each network.

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ socket.on('connect', function() {
 });
 socket.on('brain', function(result) {
   // Update paths between nodes when new weights are provided
-  console.log('#############\n ', result.iterations, '\n', result.error, '\n', result.brain);
+  // console.log('#############\n ', result.iterations, '\n', result.error, '\n', result.brain);
   
 });
 
@@ -23,13 +23,34 @@ $(document).ready(function() {
     
     e.preventDefault();
 
+    var formDataString = '[[' + $('#hiddenLayers1').val() + '],[' + $('#hiddenLayers2').val() + '],[' + $('#hiddenLayers3').val() + '],[' + $('#hiddenLayers4').val() + ']]';
+
     var formData = {
-      'hiddenLayers': $('#hiddenLayers').val()
+      'hiddenLayers': formDataString
     };
+
+    console.log('formData:', formData);
+    
+    // Render nodes for neural network architecture
+    drawNetworks(formData);
 
     socket.emit('train', formData);
     $('#hiddenLayers').val('');
     console.log('train brains!');
+
+
   });
 
 });
+
+// Render a network of nodes for neural network
+var drawNetworks = function() {
+  // Parse the values in the input forms to get the array of nodes for each network
+  // Add element for nodes in input layer and output layer
+  var network1NodeList = [2].concat($('#hiddenLayers1').val().split(',').map(Number),[0]);
+  // Add one to each layer to account for bias nodes
+  network1NodeList = network1NodeList.map(function(elem){
+    return elem + 1;
+  });
+  console.log(network1NodeList);
+};
