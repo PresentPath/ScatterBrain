932a7309a8a69c4060c79afdf9e6fddc76f9dc22
Katrina Uychaco <kuychaco@gmail.com>
2015-07-22 22:16:06 -0700

    Resolve merge conflict

--- public/js/visualize.js
+++ public/js/visualize.js
@@ -2,7 +2,7 @@
 * @Author: Katrina Uychaco
 * @Date:   2015-07-22 19:57:55
 * @Last Modified by:   Katrina Uychaco
-* @Last Modified time: 2015-07-22 20:44:14
+* @Last Modified time: 2015-07-22 22:15:31
 */
 
 'use strict';
@@ -34,9 +34,15 @@ var visualize = function(networkNum, nodePositions, links) {
     .attr('cy', function(d) { return d.y; })
     .attr('r', '10');
 
-  // var
 
+  // Create new links
+  var linksEnter = links.data(links).enter();
 
+  linksEnter.append('path')
+    .attr('class', 'link')
+    .attr('d', function(d) {
+      return diagonal({ source: d.source, target: d.target });
+    });
 
 
 };
