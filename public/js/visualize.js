68cb5aba56e33cf443c5f4498bb0e1ef066923a8
Katrina Uychaco <kuychaco@gmail.com>
2015-07-24 18:33:35 -0700

    Add function to update visualization

--- public/js/visualize.js
+++ public/js/visualize.js
@@ -2,7 +2,7 @@
 * @Author: Katrina Uychaco
 * @Date:   2015-07-22 19:57:55
 * @Last Modified by:   Katrina Uychaco
-* @Last Modified time: 2015-07-22 22:54:04
+* @Last Modified time: 2015-07-24 18:16:25
 */
 
 'use strict';
@@ -14,19 +14,25 @@ var diagonal = d3.svg.diagonal();
 
 var svgs = [];
 
+// Create svg element for each neural net
 for (var i=0; i<4; i++){
   var svg = d3.select('body').append('svg')
       .attr('width', width)
       .attr('height', height)
-    .append('g');
+    
+  svg.append('g');
+  svg.append('text').attr('class', 'netNum').text('Net Number '+(i+1)).attr('y', '600');
+  svg.append('text').text('Iterations: ').attr('y', '620').attr('class', 'numIterations');
+  svg.append('text').text('Error: ').attr('y', '640').attr('class', 'error');
   svgs.push(svg);
 }
 
+// Visualize initial state of neural net
 var visualize = function(networkNum, nodePositions, linksSource) {
-  console.log('LINKS',links);
+  //console.log('LINKS',links);
 
-  var nodes = svgs[networkNum-1].selectAll('circle');
-  var links = svgs[networkNum-1].selectAll('path');
+  var nodes = svgs[networkNum-1].select('g').selectAll('circle');
+  var links = svgs[networkNum-1].select('g').selectAll('path');
 
   // Create new links
   var linksEnter = links.data(linksSource).enter();
@@ -34,7 +40,7 @@ var visualize = function(networkNum, nodePositions, linksSource) {
   linksEnter.append('path')
     .attr('class', 'link')
     .attr('d', function(d) {
-      console.log(d);
+      //console.log(d);
       return diagonal({ source: d.source, target: d.target });
     });
 
@@ -47,4 +53,24 @@ var visualize = function(networkNum, nodePositions, linksSource) {
     .attr('cy', function(d) { return d.y; })
     .attr('r', '10');
 
+};
+
+// Update link weights and results
+var update = function(result, weights) {
+  var netNum = result.networkNum;
+
+  // Update weights
+  var maxWeight = Math.max.apply(null, weights);
+  var minWeight = Math.min.apply(null, weights);
+  var range = maxWeight - minWeight;
+  var links = svgs[netNum-1].selectAll('path');
+  links.style('stroke', function(d, i) {
+    var opacity = (weights[i]-minWeight)/range;
+    return 'rgba(30,30,30,' + opacity + ')';
+  });
+
+  // Update results
+  svgs[netNum-1].select('.numIterations').text(result.iterations);
+  //console.log(typeof result.error, '@@@@@@@@@@@@@')
+  svgs[netNum-1].select('.error').text(Math.round(1000*result.error)/1000);
 };
